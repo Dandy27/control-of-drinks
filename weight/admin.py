14acd294ed365aa659a72650942d89917372e3ad
Dandy27 <danielsbarbosa27@gmail.com>
2020-10-25 23:20:54 -0300

    Add models

---
+++ weight/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import WeightConfig
+
+# admin.site.site_header = "Inventory control "
+
+admin.site.register(WeightConfig)
---
+++ weight/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class WeightConfig(models.Model):
+    name_product = models.CharField(max_length=100)
+    weight_liquid = models.DecimalField(max_digits=6, decimal_places=1, help_text='GRS')
+    quantity_of_doses = models.DecimalField(max_digits=6, decimal_places=1, help_text='QUANT')
+    gross_bottle_weight = models.DecimalField(max_digits=6, decimal_places=1, help_text='GRS')
+    tare = models.DecimalField(max_digits=6, decimal_places=1, help_text='GRS')
+    weigh_bottle_and_liquid = models.DecimalField(max_digits=6, decimal_places=1, help_text='Barman')
+
+
+
+
+    def __str__(self):
+        return self.name_product
